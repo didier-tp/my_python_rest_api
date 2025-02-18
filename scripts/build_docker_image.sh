4ac0c23a553caf6583b82edab87e1a0a514db836
DidierDefrance <didier@d-defrance.fr>
2025-02-18 10:38:29 +0100

    s

--- scripts/build_docker_image.sh
+++ scripts/build_docker_image.sh
@@ -1,3 +1,12 @@
 cd ..
 sudo docker build -t my_rest_api:1 .
-sudo docker tag my_rest_api:1 didierDefrance69/my_rest_api:1
+#docker tag my_rest_api:1 didierdefrance69/my_rest_api:1
+
+#docker login -u didierdefrance69 (with password)
+#docker push didierdefrance69/my_rest_api:1
+
+
+
+#docker image rm didierdefrance69/my_rest_api:1
+#docker image rm my_rest_api:1
+#docker pull didierdefrance69/my_rest_api:1
