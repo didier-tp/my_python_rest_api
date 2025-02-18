a046f00c9ff9eb01874952d77fbe5ecb5c10b42b
DidierDefrance <didier@d-defrance.fr>
2025-02-18 09:38:09 +0100

    s

--- scripts/venv_test.sh
+++ scripts/venv_test.sh
@@ -6,8 +6,9 @@ python -m venv .venv
 #activate virtualenv:
 . .venv/bin/activate
 
-#pip in venv:
-python -m pip install -r requirements.txt --user
+#pip in venv (sudo not advised , only if necessary):
+#python -m pip install -r requirements.txt
+sudo python -m pip install -r requirements.txt
 
 #run test
 pytest --junit-xml test-reports/results.xml test_devise_api.py
