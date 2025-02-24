5e2f0cb25c6390174b4660bd6a4a8c5534f7a2ae
Didier_Defrance <didier@d-defrance.fr>
2025-02-24 13:36:02 +0100

    s

--- essai_sql.py
+++ essai_sql.py
@@ -2,7 +2,7 @@ import sqlite3
 
 with sqlite3.connect("devises.db") as connection:
     cursor = connection.cursor()
-    
+    '''
     cursor.execute(
         "CREATE TABLE devise (code TEXT PRIMARY KEY ,name TEXT, change DOUBLE)")
     connection.commit()
@@ -10,7 +10,7 @@ with sqlite3.connect("devises.db") as connection:
     cursor.execute(
         "INSERT INTO devise(code, name, change) VALUES('CCC', 'devise_ccc', 123.456)")
     connection.commit()
-
+    '''
     rows = cursor.execute(
         "SELECT * FROM devise").fetchall() #or .fetchone()
     print(rows)
